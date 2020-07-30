bcc7722c7fb6e96899a6a6b3eb5c59cf3a721d10
emeka ewelike <emekaewelike@gmail.com>
2020-07-30 14:37:37 +0100

    created modules

--- educa/settings.py
+++ educa/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'educa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR),'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,4 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+    ]
+
+#customize the redirect after login
+LOGIN_REDIRECT_URL = 'manage_course_list'
