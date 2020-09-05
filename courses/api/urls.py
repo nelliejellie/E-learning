8e139649c680f30a32517404bc71f762c81bbe51
emeka ewelike <emekaewelike@gmail.com>
2020-09-05 15:23:59 +0100

    updated serialisers

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('subjects/', 
         views.SubjectListView.as_view(),
         name='subject_list'),
-
     path('subjects/<pk>/', 
         views.SubjectDetailView.as_view(),
         name='subject_detail'),
-    
+    path('courses/<pk>/enroll/', 
+        views.CourseEnrollView.as_view(),
+        name='course_enroll'),
 ]
--- courses/api/views.py
+++ courses/api/views.py
@@ -1,11 +1,26 @@
-from rest_framework import generics 
-from ..models import Subject
+from rest_framework import generics
+from ..models import Subject, Course
 from .serializers import SubjectSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response 
+# importing authentication for a view
+from rest_framework.authentication import BasicAuthentication
 
+# view for all the subject fields
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
+# view for all the subject detail
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+# view for all students enrollment
+class CourseEnrollView(APIView):
+    authentication_classes = (BasicAuthentication,)
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled':True})
